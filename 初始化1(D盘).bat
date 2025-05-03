bd4ebcad8f241b9da8c5cfda4e1d6e47ff3686f5
jackhu5 <151327744+jackhu5@users.noreply.github.com>
2025-05-03 13:25:28 +0800

    Update 初始化1(D盘).bat

--- 初始化1(D盘).bat
+++ 初始化1(D盘).bat
@@ -1,11 +1,11 @@
 @echo off
 color F0
 mode con cols=58 lines=30
-title=m4a to mp3 v1.0 :By:Bilibili@United-States__
+title=m4a to mp3 v1.0
 if not exist D:\ffmpeg (
 cd /d %~dp0
 xcopy ffmpeg D:\ /E/Y
 setx path "D:\ffmpeg\bin"
-echo 初始化成功！)
-if exist D:\ffmpeg (echo 无需重复初始化！)
+echo 鍒濆鍖栨垚鍔燂紒)
+if exist D:\ffmpeg (echo 鏃犻渶閲嶅鍒濆鍖栵紒)
 pause
